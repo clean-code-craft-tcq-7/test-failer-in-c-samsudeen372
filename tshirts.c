ddf05f976a02bbfd48d316fbcbb0f9142e46cbdd
Sudeep Prasad <sudeep.rp@gmail.com>
2022-01-11 23:19:04 +0530

    prob-defn

---
+++ tshirts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+
+char size(int cms) {
+    if(cms < 38) {
+        return 'S';
+    } else if(cms > 38 && cms < 42) {
+        return 'M';
+    } else if(cms > 42) {
+        return 'L';
+    }
+}
+
+void main() {
+    assert(size(37) == 'S');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+}
