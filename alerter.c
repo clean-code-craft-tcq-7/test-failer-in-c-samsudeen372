3c301fb1db10e5f277f793666997465ac198a317
Sudeep Prasad <sudeep.rp@gmail.com>
2022-01-19 21:45:38 +0530

    challenge

--- alerter.c
+++ alerter.c
@@ -5,8 +5,8 @@ int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for success
-    // Return 500 for failure
+    // Return 200 for ok
+    // Return 500 for not-ok
     // stub always succeeds and returns 200
     return 200;
 }
@@ -15,7 +15,10 @@ void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
-        // When there is a non-ok response, the code doesn't work as expected
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
 }
